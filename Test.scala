76640106c9b25f8c41425a52c4465ba5dfea62da
Saurav <saurav.c@comakeit.com>
2016-04-11 12:26:43 +0000

    Testing out if statement

---
+++ Test.scala
@@ -0,0 +1,16 @@
+// Test if statement
+//
+
+object Test {
+  def main(args: Array[String]){
+    var x = 30;
+    var y = 20;
+
+    if ( x == 30 ) {
+      if ( y == 20  )
+      {
+        println("X = 30 and Y = 20")
+      }
+    }
+  }
+}
